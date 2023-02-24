7c83e9670ede40c092d078c0a1aa17bdacb20f36
Abner Silva <abner.js05@gmail.com>
2023-02-24 19:35:56 -0300

    magnetic button

---
+++ src/components/MagneticButton/MagneticButton.tsx
@@ -0,0 +1,46 @@
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import './MagneticButton.css'
+
+interface Props {
+    onClick?: any;
+    className?: string;
+    endIcon?: any;
+    children?: any;
+    variant?: 'contained' | 'outlined' | 'text' | undefined;
+}
+
+const MagneticButton = (props: Props) => {
+    const [translate, setTranslate] = useState('translate(0,0)')
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const btn = e.target as HTMLElement;
+        const position = btn.getBoundingClientRect();
+        const x = e.clientX - position.left - position.width / 2;
+        const y = e.clientY - position.top - position.height / 2;
+        setTranslate(`translate(${x * 0.3}px, ${y * 0.5}px)`)
+    }
+
+    const handleMouseLeave = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setTranslate('translate(0,0)')
+    }
+
+    return (
+        <Button
+            className={props.className + ' follows-btn'}
+            variant={props.variant || 'contained'}
+            disableElevation
+            onClick={props.onClick}
+            endIcon={props.endIcon}
+            onMouseMove={(e) => handleMouseMove(e)}
+            style={{
+                transform: translate
+            }}
+            onMouseLeave={handleMouseLeave}
+        >
+            {props.children}
+        </Button>
+    );
+}
+
+export default MagneticButton;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,28 @@
 import { Avatar, Button } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from 'src/components/Navbar/Navbar';
 import './home.css';
 import { Icon } from '@iconify/react';
+import photo from '../assets/images/imgLanding.png';
+import { Link } from 'react-router-dom';
+import MagneticButton from 'src/components/MagneticButton/MagneticButton';
 
 const Home = () => {
+    const [translate, setTranslate] = useState('translate(0,0)')
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const btn = e.target as HTMLElement;
+        const position = btn.getBoundingClientRect();
+        const x = e.clientX - position.left - position.width / 2;
+        const y = e.clientY - position.top - position.height / 2;
+        setTranslate(`translate(${x * 0.3}px, ${y * 0.5}px)`)
+    }
+
+    const handleMouseLeave = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setTranslate('translate(0,0)')
+    }
+
+
     return (
         <div className="home">
             <Navbar />
@@ -29,8 +47,14 @@ const Home = () => {
             </div>
 
             <div className="curriculum">
-                <Button variant='contained' disableElevation endIcon={<Icon icon="fluent:arrow-download-16-filled" />}>
-                    BAIXAR<br />CURRICULO
+                <Button
+                    variant='contained'
+                    disableElevation
+                    endIcon={<Icon icon="fluent:arrow-download-16-filled" />}
+                >
+                    <a href="../assets/AbnerCurriculumVitae.pdf" target="_blank" download>
+                        BAIXAR<br />CURRÍCULO
+                    </a>
                 </Button>
             </div>
 
@@ -50,9 +74,7 @@ const Home = () => {
                     <div className="text">
                         A combinação da minha paixão por design, código e interações.
                     </div>
-                    <Button variant='contained' disableElevation>
-                        Sobre mim
-                    </Button>
+                    <MagneticButton>Sobre mim</MagneticButton>
                 </div>
             </div>
 
@@ -62,7 +84,7 @@ const Home = () => {
                 <div className="content">
                     <div className="text">Portal de Relatórios</div>
                     <div className="info">
-                        <div className="type">React</div>
+                        <div className="type">React + .NET</div>
                         <div className="dev">Design & Desenvolvimento</div>
                     </div>
                 </div>
@@ -84,17 +106,21 @@ const Home = () => {
                 </div>
                 <hr />
                 <div className="content">
-                    <div className="text">Portal de Relatórios</div>
+                    <div className="text">Identidade Visual</div>
                     <div className="info">
-                        <div className="type">React</div>
-                        <div className="dev">Design & Desenvolvimento</div>
+                        <div className="type">Photoshop</div>
+                        <div className="dev">Design</div>
                     </div>
                 </div>
                 <hr />
-                <div className="more-work">
-                    <Button variant='outlined' className='moreWork' endIcon={<Icon icon="fluent:arrow-right-48-regular" />}>
+                <div className="more-work">                    
+                    <MagneticButton 
+                        className='moreWork'
+                        variant='outlined'
+                        endIcon={<Icon icon="fluent:arrow-right-48-regular" />}
+                    >
                         Mais trabalhos
-                    </Button>
+                    </MagneticButton>
                 </div>
 
 
@@ -115,14 +141,48 @@ const Home = () => {
             </div>
 
             <div className="contact">
-                <div className="title">
-                    <h2>
-                        <span>
-                            <Avatar alt="Abner Silva" src="../assets/images/imgLanding.png" />
-                            Vamos trabalhar
-                        </span>
-                        <span>juntos!</span>
-                    </h2>
+                <div className="container">
+                    <div className="title">
+                        <h2>
+                            <div>
+                                <Avatar
+                                    alt="Abner Silva"
+                                    src={photo}
+                                    sx={{
+                                        bgcolor: 'darkgrey',
+                                        width: 100, height: 100,
+                                        marginRight: 4
+                                    }}
+                                />
+                                Vamos
+                            </div>
+                            <div>trabalhar juntos!</div>
+                        </h2>
+                    </div>
+                    <hr />
+                    <div className="contact-btn">
+                        <div className="float-btn">
+                        <MagneticButton className='elementBtn'>Sobre mim</MagneticButton>
+                        </div>
+                    </div>
+                    <div className="infos">
+                        <MagneticButton variant='outlined'>abner.js05@gmail.com</MagneticButton>
+                        <MagneticButton variant='outlined'>+55 18 99736-1645</MagneticButton>
+                    </div>
+                </div>
+                <div className="footer">
+                    <div className="copy">
+                        © 2023 - Desenvolvido por Abner J. Silva
+                    </div>
+                    <div className="socials">
+                        <div className="title">REDES SOCIAIS</div>
+                        <div className="links">
+                            <Link to={{ pathname: 'https://wa.me/5518988189353' }} target="_blank">WhatsApp</Link>
+                            <Link to={{ pathname: 'https://www.instagram.com/eae.abner/' }} target="_blank">Instagram</Link>
+                            <Link to={{ pathname: 'https://www.linkedin.com/in/abner-j-silva/' }} target="_blank">LinkedIn</Link>
+                            <Link to={{ pathname: 'https://www.behance.net/abnerjsilva' }} target="_blank">Behance</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
