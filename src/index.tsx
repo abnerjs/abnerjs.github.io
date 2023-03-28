bfeb5a6b65190ffc08f0ad69f66c4d8a6371a694
Abner Silva <abner.js05@gmail.com>
2023-03-28 20:39:42 -0300

    meta tags

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
@@ -8,11 +9,14 @@ import { ThemeProvider } from "@mui/material";
 import theme from "./assets/theme";
 import "./assets/main.css";
 
-ReactDOM.render(
+const container = document.getElementById("root") as HTMLElement;
+
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
