4db41cfe47bae898898a4f89c48667cebed978f8
Abner Silva <abner.js05@gmail.com>
2024-09-25 16:34:06 -0300

    docs: att cv pdf, add some projects, fix favicon

--- src/pages/Home/Works/Works.tsx
+++ src/pages/Home/Works/Works.tsx
@@ -1,311 +1,313 @@
-import { useRef, useState } from "react";
-import "./works.css";
-import { Icon } from "@iconify/react";
-import { Parallax } from "react-scroll-parallax";
-import MagneticButton from "src/components/MagneticButton/MagneticButton";
-import CustomCursor from "src/components/CustomCursor/CustomCursor";
-import { useNavigate } from "react-router-dom";
-import integra from "src/assets/images/works/integra30.png";
-import ponto from "src/assets/images/works/ponto.png";
-import swingmd from "src/assets/images/works/swingmd.gif";
-import insumos from "src/assets/images/works/insumos.png";
-import iworkoff from "src/assets/images/works/iworkoff.gif";
-import portalRelat from "src/assets/images/works/portal.png";
-import useTransitionStore from "src/store/storeConfig";
-import MenuContext from "src/components/MenuContext/MenuContext";
+import { useRef, useState } from 'react'
+import './works.css'
+import { Icon } from '@iconify/react'
+import { Parallax } from 'react-scroll-parallax'
+import MagneticButton from 'src/components/MagneticButton/MagneticButton'
+import CustomCursor from 'src/components/CustomCursor/CustomCursor'
+import { useNavigate } from 'react-router-dom'
+import integra from 'src/assets/images/works/integra30.png'
+import ponto from 'src/assets/images/works/ponto.png'
+import swingmd from 'src/assets/images/works/swingmd.gif'
+import insumos from 'src/assets/images/works/insumos.png'
+import iworkoff from 'src/assets/images/works/iworkoff.gif'
+import viacepflutter from 'src/assets/images/works/viacepflutter.png'
+import portalRelat from 'src/assets/images/works/portal.png'
+import bcTasksDio from 'src/assets/images/works/bcTasksDio.gif'
+import useTransitionStore from 'src/store/storeConfig'
+import MenuContext from 'src/components/MenuContext/MenuContext'
 
 const HoverPanel = (props: any) => {
   return (
     <div
-      className="hover-panel"
+      className='hover-panel'
       style={{
         width: 500,
         height: 500,
-        overflow: "hidden",
+        overflow: 'hidden',
       }}
     >
-      <MagneticButton className="access">Acessar</MagneticButton>
+      <MagneticButton className='access'>Acessar</MagneticButton>
       <div
-        className="panel"
+        className='panel'
         style={{
-          width: "100%",
-          height: "400%",
+          width: '100%',
+          height: '400%',
           transform: `translateY(-${25 * props.hoverIndex}%)`,
-          transition: "all ease 0.4s",
+          transition: 'all ease 0.4s',
         }}
       >
         <div
-          className="subPanel"
+          className='subPanel'
           style={{
-            width: "100%",
-            height: "25%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#C3CCE1",
+            width: '100%',
+            height: '25%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#C3CCE1',
           }}
         >
           <img
             src={portalRelat}
             style={{
-              width: "80%",
+              width: '80%',
             }}
-            alt="Portal de Relatórios"
+            alt='Portal de Relatórios'
           />
         </div>
         <div
-          className="subPanel"
+          className='subPanel'
           style={{
-            width: "100%",
-            height: "25%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#D4B492",
+            width: '100%',
+            height: '25%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#D4B492',
           }}
         >
           <img
             src={swingmd}
             style={{
-              width: "80%",
+              width: '80%',
             }}
-            alt="Java Swing Material Design"
+            alt='Java Swing Material Design'
           />
         </div>
         <div
-          className="subPanel"
+          className='subPanel'
           style={{
-            width: "100%",
-            height: "25%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#D4CAC6",
+            width: '100%',
+            height: '25%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#D4CAC6',
           }}
         >
           <img
             src={ponto}
             style={{
-              width: "80%",
+              width: '80%',
             }}
-            alt="Plataforma de Ponto"
+            alt='Plataforma de Ponto'
           />
         </div>
         <div
-          className="subPanel"
+          className='subPanel'
           style={{
-            width: "100%",
-            height: "25%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#57A6A2",
+            width: '100%',
+            height: '25%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#57A6A2',
           }}
         >
           <img
             src={integra}
             style={{
-              width: "80%",
+              width: '80%',
             }}
-            alt="Integra"
+            alt='Integra'
           />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 const WorksContent = (props: any) => {
-  const el1 = useRef<HTMLDivElement>(null);
-  const el2 = useRef<HTMLDivElement>(null);
-  const el3 = useRef<HTMLDivElement>(null);
-  const el4 = useRef<HTMLDivElement>(null);
+  const elemPortalRelat = useRef<HTMLDivElement>(null)
+  const elemSwingMaterial = useRef<HTMLDivElement>(null)
+  const elemPonto = useRef<HTMLDivElement>(null)
+  const elemParty = useRef<HTMLDivElement>(null)
   return (
     <>
       <div
-        ref={el1}
-        className="content"
+        ref={elemPortalRelat}
+        className='content'
         onMouseEnter={() => {
-          props.setHoverIndex(0);
-          props.setScale(1);
+          props.setHoverIndex(0)
+          props.setScale(1)
         }}
         onMouseLeave={() => props.setScale(0)}
         onClick={() => {
-          window.open("https://github.com/abnerjs/PortalRelatorios");
+          window.open('https://github.com/abnerjs/PortalRelatorios')
           window.open(
-            "https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1",
-            "blank"
-          );
+            'https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1',
+            'blank'
+          )
         }}
         onMouseDown={(e) => {
           if (e.button === 1) {
-            window.open("https://github.com/abnerjs/PortalRelatorios", "blank");
+            window.open('https://github.com/abnerjs/PortalRelatorios', 'blank')
             window.open(
-              "https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1",
-              "blank"
-            );
+              'https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1',
+              'blank'
+            )
           }
         }}
       >
         <MenuContext
-          targetId={el1.current!}
-          link={"https://github.com/abnerjs/PortalRelatorios"}
+          targetId={elemPortalRelat.current!}
+          link={'https://github.com/abnerjs/PortalRelatorios'}
           link2={
-            "https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1"
+            'https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1'
           }
         />
-        <div className="text">Portal de Relatórios</div>
-        <div className="info">
-          <div className="type">React + .NET</div>
-          <div className="dev">Design & Desenvolvimento</div>
+        <div className='text'>Portal de Relatórios</div>
+        <div className='info'>
+          <div className='type'>React + .NET</div>
+          <div className='dev'>Design & Desenvolvimento</div>
         </div>
-        <div className="container"></div>
+        <div className='container'></div>
       </div>
 
       <hr />
 
       <div
-        ref={el2}
-        className="content"
+        ref={elemSwingMaterial}
+        className='content'
         onMouseEnter={() => {
-          props.setHoverIndex(1);
-          props.setScale(1);
+          props.setHoverIndex(1)
+          props.setScale(1)
         }}
         onMouseLeave={() => props.setScale(0)}
         onClick={() =>
-          window.open("https://github.com/abnerjs/SwingMaterialDesign")
+          window.open('https://github.com/abnerjs/SwingMaterialDesign')
         }
         onMouseDown={(e) => {
           if (e.button === 1) {
             window.open(
-              "https://github.com/abnerjs/SwingMaterialDesign",
-              "blank"
-            );
+              'https://github.com/abnerjs/SwingMaterialDesign',
+              'blank'
+            )
           }
         }}
       >
         <MenuContext
-          targetId={el2.current!}
-          link={"https://github.com/abnerjs/SwingMaterialDesign"}
+          targetId={elemSwingMaterial.current!}
+          link={'https://github.com/abnerjs/SwingMaterialDesign'}
         />
-        <div className="text">Material Design</div>
-        <div className="info">
-          <div className="type">Java Swing</div>
-          <div className="dev">Desenvolvimento de Componentes</div>
+        <div className='text'>Material Design</div>
+        <div className='info'>
+          <div className='type'>Java Swing</div>
+          <div className='dev'>Desenvolvimento de Componentes</div>
         </div>
       </div>
 
       <hr />
 
       <div
-        ref={el3}
-        className="content mobile"
+        ref={elemPonto}
+        className='content mobile'
         onMouseEnter={() => {
-          props.setHoverIndex(2);
-          props.setScale(1);
+          props.setHoverIndex(2)
+          props.setScale(1)
         }}
         onMouseLeave={() => props.setScale(0)}
         onClick={() => {
           window.open(
-            "https://www.figma.com/file/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=0%3A1&t=DfzvDMfDG6r6y8yf-1"
-          );
+            'https://www.figma.com/file/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=0%3A1&t=DfzvDMfDG6r6y8yf-1'
+          )
           window.open(
-            "https://www.figma.com/file/PUN3JlrbyJfbcJg6Pll1FZ/Plataforma-de-Ponto?node-id=0%3A1&t=dBuqpKSyFkH2JWJY-1",
-            "blank"
-          );
+            'https://www.figma.com/file/PUN3JlrbyJfbcJg6Pll1FZ/Plataforma-de-Ponto?node-id=0%3A1&t=dBuqpKSyFkH2JWJY-1',
+            'blank'
+          )
         }}
         onMouseDown={(e) => {
           if (e.button === 1) {
             window.open(
-              "https://www.figma.com/file/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=0%3A1&t=DfzvDMfDG6r6y8yf-1",
-              "blank"
-            );
+              'https://www.figma.com/file/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=0%3A1&t=DfzvDMfDG6r6y8yf-1',
+              'blank'
+            )
             window.open(
-              "https://www.figma.com/file/PUN3JlrbyJfbcJg6Pll1FZ/Plataforma-de-Ponto?node-id=0%3A1&t=dBuqpKSyFkH2JWJY-1",
-              "blank"
-            );
+              'https://www.figma.com/file/PUN3JlrbyJfbcJg6Pll1FZ/Plataforma-de-Ponto?node-id=0%3A1&t=dBuqpKSyFkH2JWJY-1',
+              'blank'
+            )
           }
         }}
       >
         <MenuContext
-          targetId={el3.current!}
+          targetId={elemPonto.current!}
           link={
-            "https://www.figma.com/file/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=0%3A1&t=DfzvDMfDG6r6y8yf-1"
+            'https://www.figma.com/file/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=0%3A1&t=DfzvDMfDG6r6y8yf-1'
           }
           link2={
-            "https://www.figma.com/file/PUN3JlrbyJfbcJg6Pll1FZ/Plataforma-de-Ponto?node-id=0%3A1&t=dBuqpKSyFkH2JWJY-1"
+            'https://www.figma.com/file/PUN3JlrbyJfbcJg6Pll1FZ/Plataforma-de-Ponto?node-id=0%3A1&t=dBuqpKSyFkH2JWJY-1'
           }
         />
-        <div className="text">Sistema de Ponto</div>
-        <div className="info">
-          <div className="type">Figma</div>
-          <div className="dev">Design & Interação</div>
+        <div className='text'>Sistema de Ponto</div>
+        <div className='info'>
+          <div className='type'>Figma</div>
+          <div className='dev'>Design & Interação</div>
         </div>
       </div>
 
-      <hr className="mobile" />
+      <hr className='mobile' />
 
       <div
-        ref={el4}
-        className="content mobile"
+        ref={elemParty}
+        className='content mobile'
         onMouseEnter={() => {
-          props.setHoverIndex(3);
-          props.setScale(1);
+          props.setHoverIndex(3)
+          props.setScale(1)
         }}
         onMouseLeave={() => props.setScale(0)}
         onClick={() =>
           window.open(
-            "https://www.behance.net/gallery/95516641/Integra-party-corona"
+            'https://www.behance.net/gallery/95516641/Integra-party-corona'
           )
         }
         onMouseDown={(e) => {
           if (e.button === 1) {
             window.open(
-              "https://www.behance.net/gallery/95516641/Integra-party-corona",
-              "blank"
-            );
+              'https://www.behance.net/gallery/95516641/Integra-party-corona',
+              'blank'
+            )
           }
         }}
       >
         <MenuContext
-          targetId={el4.current!}
-          link={"https://www.behance.net/gallery/95516641/Integra-party-corona"}
+          targetId={elemParty.current!}
+          link={'https://www.behance.net/gallery/95516641/Integra-party-corona'}
         />
-        <div className="text">Identidade Visual</div>
-        <div className="info">
-          <div className="type">Photoshop</div>
-          <div className="dev">Design</div>
+        <div className='text'>Identidade Visual</div>
+        <div className='info'>
+          <div className='type'>Photoshop</div>
+          <div className='dev'>Design</div>
         </div>
       </div>
 
-      <hr className="mobile" />
+      <hr className='mobile' />
     </>
-  );
-};
+  )
+}
 
 const Works = () => {
-  const changeTransition = useTransitionStore((state) => state.change);
-  const changeLabel = useTransitionStore((state) => state.changeLabel);
-  const navigate = useNavigate();
-  const [scale, setScale] = useState(0);
-  const [hoverIndex, setHoverIndex] = useState(0);
+  const changeTransition = useTransitionStore((state) => state.change)
+  const changeLabel = useTransitionStore((state) => state.changeLabel)
+  const navigate = useNavigate()
+  const [scale, setScale] = useState(0)
+  const [hoverIndex, setHoverIndex] = useState(0)
 
   const handlerGoToWorks = () => {
-    changeTransition(1);
-    changeLabel("• TRABALHOS •");
+    changeTransition(1)
+    changeLabel('• TRABALHOS •')
 
     const timer = setTimeout(() => {
-      navigate("/works");
-    }, 500);
+      navigate('/works')
+    }, 500)
     return () => {
-      clearTimeout(timer);
-    };
-  };
+      clearTimeout(timer)
+    }
+  }
 
   return (
     <>
-      <div className="works">
-        <div className="title">Principais trabalhos</div>
+      <div className='works'>
+        <div className='title'>Principais trabalhos</div>
         <hr />
 
         <CustomCursor scale={scale}>
@@ -314,11 +316,11 @@ const Works = () => {
 
         <WorksContent setScale={setScale} setHoverIndex={setHoverIndex} />
 
-        <div className="more-work">
+        <div className='more-work'>
           <MagneticButton
-            className="moreWork"
-            variant="outlined"
-            endIcon={<Icon icon="fluent:arrow-right-48-regular" />}
+            className='moreWork'
+            variant='outlined'
+            endIcon={<Icon icon='fluent:arrow-right-48-regular' />}
             onClick={handlerGoToWorks}
           >
             Mais trabalhos
@@ -326,30 +328,34 @@ const Works = () => {
         </div>
       </div>
 
-      <div className="works-grid">
-        <Parallax translateX={["0%", "-20%"]}>
-          <div className="row">
-            <div className="work"></div>
-            <div className="work">
-              <img src={insumos} alt="Insumos" />
+      <div className='works-grid'>
+        <Parallax translateX={['0%', '-20%']}>
+          <div className='row'>
+            <div className='work'></div>
+            <div className='work'>
+              <img src={insumos} alt='Insumos' />
             </div>
-            <div className="work"></div>
-            <div className="work"></div>
+            <div className='work'>
+              <img src={viacepflutter} alt='ViaCEP Flutter' />
+            </div>
+            <div className='work'></div>
           </div>
         </Parallax>
-        <Parallax translateX={["0%", "20%"]}>
-          <div className="row reverse">
-            <div className="work"></div>
-            <div className="work"></div>
-            <div className="work">
-              <img src={iworkoff} alt="iWorkOff" />
+        <Parallax translateX={['0%', '20%']}>
+          <div className='row reverse'>
+            <div className='work'></div>
+            <div className='work'></div>
+            <div className='work'>
+              <img src={iworkoff} alt='iWorkOff' />
             </div>
-            <div className="work"></div>
+            <div className='work'>
+              <img src={bcTasksDio} alt='BCTasksDio' />
+            </div>
           </div>
         </Parallax>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Works;
+export default Works
--- src/pages/Works/WorksContainer/WorksContainer.tsx
+++ src/pages/Works/WorksContainer/WorksContainer.tsx
@@ -1,104 +1,123 @@
-import { useState } from "react";
-import "./works-container.css";
-import CustomCursor from "src/components/CustomCursor/CustomCursor";
-import WorkItem from "./WorkItem/WorkItem";
-import { useLocation, useNavigate } from "react-router-dom";
-import integra from "src/assets/images/works/integra30.png";
-import insumos from "src/assets/images/works/insumos.png";
-import ponto from "src/assets/images/works/ponto.png";
-import swingmd from "src/assets/images/works/swingmd.gif";
-import iworkoff from "src/assets/images/works/iworkoff.gif";
-import portalRelat from "src/assets/images/works/portal.png";
-import useTransitionStore from "src/store/storeConfig";
+import { useState } from 'react'
+import './works-container.css'
+import CustomCursor from 'src/components/CustomCursor/CustomCursor'
+import WorkItem from './WorkItem/WorkItem'
+import { useLocation, useNavigate } from 'react-router-dom'
+import integra from 'src/assets/images/works/integra30.png'
+import insumos from 'src/assets/images/works/insumos.png'
+import ponto from 'src/assets/images/works/ponto.png'
+import swingmd from 'src/assets/images/works/swingmd.gif'
+import iworkoff from 'src/assets/images/works/iworkoff.gif'
+import bcTasksDio from 'src/assets/images/works/bcTasksDio.gif'
+import portalRelat from 'src/assets/images/works/portal.png'
+import viacepflutter from 'src/assets/images/works/viacepflutter.png'
+import useTransitionStore from 'src/store/storeConfig'
 
 const WorksContainer = () => {
-  const changeTransition = useTransitionStore((state) => state.change);
-  const changeLabel = useTransitionStore((state) => state.changeLabel);
-  const [scale, setScale] = useState(0);
-  const location = useLocation();
-  const navigate = useNavigate();
+  const changeTransition = useTransitionStore((state) => state.change)
+  const changeLabel = useTransitionStore((state) => state.changeLabel)
+  const [scale, setScale] = useState(0)
+  const location = useLocation()
+  const navigate = useNavigate()
 
   // use in future: page for each work
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handlerGoTo = (url: string, label: string) => {
-    if (location.pathname === url) return;
-    changeTransition(1);
-    changeLabel(`• ${label} •`);
+    if (location.pathname === url) return
+    changeTransition(1)
+    changeLabel(`• ${label} •`)
 
     const timer = setTimeout(() => {
-      navigate(url);
-    }, 500);
+      navigate(url)
+    }, 500)
     return () => {
-      clearTimeout(timer);
-    };
-  };
+      clearTimeout(timer)
+    }
+  }
 
   return (
-    <div className="works-container">
+    <div className='works-container'>
       <CustomCursor scale={scale}>
-        <div className="btn-hover-view">
-          <div className="text">Ver</div>
+        <div className='btn-hover-view'>
+          <div className='text'>Ver</div>
         </div>
       </CustomCursor>
       <WorkItem
-        title="Portal de Relatórios"
-        description="Design & Desenvolvimento"
-        dev="React"
-        year="2022"
+        title='Portal de Relatórios'
+        description='Design & Desenvolvimento'
+        dev='React'
+        year='2022'
         panel={portalRelat}
         setScale={setScale}
-        link="https://github.com/abnerjs/PortalRelatorios"
-        link2="https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1"
+        link='https://github.com/abnerjs/PortalRelatorios'
+        link2='https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1'
       />
       <WorkItem
-        title="iWorkOff"
-        description="Design & Desenvolvimento"
-        dev="React"
-        year="2021"
+        title='iWorkOff'
+        description='Design & Desenvolvimento'
+        dev='React'
+        year='2021'
         panel={iworkoff}
         setScale={setScale}
-        link="https://github.com/abnerjs/iworkoff"
-        link2="https://www.figma.com/file/2TfY45k5qHg3uvEfy3P7dQ/iWorkOff?node-id=0%3A1&t=vnb6fSMXurPUZAQJ-1"
+        link='https://github.com/abnerjs/iworkoff'
+        link2='https://www.figma.com/file/2TfY45k5qHg3uvEfy3P7dQ/iWorkOff?node-id=0%3A1&t=vnb6fSMXurPUZAQJ-1'
       />
       <WorkItem
-        title="Material Design"
-        description="Design & Desenvolvimento"
-        dev="Java Swing"
-        year="2019"
+        title='ViaCep Flutter'
+        description='Design & Desenvolvimento'
+        dev='Flutter'
+        year='2023'
+        panel={viacepflutter}
+        setScale={setScale}
+        link='https://github.com/abnerjs/viacep'
+      />
+      <WorkItem
+        title='Material Design'
+        description='Design & Desenvolvimento'
+        dev='Java Swing'
+        year='2019'
         panel={swingmd}
         setScale={setScale}
-        link="https://github.com/abnerjs/SwingMaterialDesign"
+        link='https://github.com/abnerjs/SwingMaterialDesign'
       />
       <WorkItem
-        title="Sistema de Ponto"
-        description="Design & Interação"
-        dev="Figma"
-        year="2022"
+        title='Sistema de Ponto'
+        description='Design & Interação'
+        dev='Figma'
+        year='2022'
         panel={ponto}
         setScale={setScale}
-        link="https://www.figma.com/file/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=0%3A1&t=DfzvDMfDG6r6y8yf-1"
-        link2="https://www.figma.com/file/PUN3JlrbyJfbcJg6Pll1FZ/Plataforma-de-Ponto?node-id=0%3A1&t=dBuqpKSyFkH2JWJY-1"
+        link='https://www.figma.com/file/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=0%3A1&t=DfzvDMfDG6r6y8yf-1'
       />
       <WorkItem
-        title="Identidade Visual"
-        description="Design"
-        dev="Photoshop"
-        year="2020"
+        title='Tasks Flutter'
+        description='Design & Desenvolvimento'
+        dev='Flutter'
+        year='2023'
+        panel={bcTasksDio}
+        setScale={setScale}
+        link='https://github.com/abnerjs/bc-flutter-app'
+      />
+      <WorkItem
+        title='Identidade Visual'
+        description='Design'
+        dev='Photoshop'
+        year='2020'
         panel={integra}
         setScale={setScale}
-        link="https://www.behance.net/gallery/95516641/Integra-party-corona"
+        link='https://www.behance.net/gallery/95516641/Integra-party-corona'
       />
       <WorkItem
-        title="Insumos"
-        description="Design & Interação"
-        dev="Figma"
-        year="2022"
+        title='Insumos'
+        description='Design & Interação'
+        dev='Figma'
+        year='2022'
         panel={insumos}
         setScale={setScale}
-        link="https://www.figma.com/file/EiV65w3Y2q1hC4HoilnL2m/Insumos?node-id=0%3A1&t=RLXFk325dwOK6C88-1"
+        link='https://www.figma.com/file/EiV65w3Y2q1hC4HoilnL2m/Insumos?node-id=0%3A1&t=RLXFk325dwOK6C88-1'
       />
     </div>
-  );
-};
+  )
+}
 
-export default WorksContainer;
+export default WorksContainer
