92f69d548b4dd46954d5601d37d83fa2f0b53fb3
abnerjs <abner.js05@gmail.com>
2024-10-30 15:29:45 -0300

    feat: ImageStack components working for 2 desktop views

--- src/pages/Works/WorksContainer/WorkItem/ImageStack/ImageStack.tsx
+++ src/pages/Works/WorksContainer/WorkItem/ImageStack/ImageStack.tsx
@@ -1,21 +1,45 @@
+import { useEffect, useState } from 'react'
+
 interface Props {
   images: string[]
   type: 'desktop' | 'mobile' | 'both'
 }
 
 const ImageStack = (props: Props) => {
+  const [transformArgs, setTransformArgs] = useState('')
+
+  useEffect(() => {
+    if (props.images.length === 2) setTransformArgs('transform scale-[0.85]')
+    if (props.images.length === 3) setTransformArgs('transform scale-[0.7]')
+  }, [])
+
   return (
-    <div className='img relative justify-center items-center flex flex-col'>
-      {props.images.map((img, index) => (
-        <div
-          className={`absolute left-[${index * 100}px] z-[${
-            10 * index
-          }] flex w-full h-full bg-contain bg-no-repeat bg-center`}
-          style={{
-            backgroundImage: `url(${img})`,
-          }}
-        ></div>
-      ))}
+    <div className='img relative'>
+      {props.images.map((img, index) => {
+        let transformOrigin = ''
+        let zIndex = ''
+
+        if (props.type === 'desktop') {
+          if (props.images.length === 2) {
+            if (index === 0) {
+              transformOrigin = 'origin-top-left'
+              zIndex = 'z-10'
+            } else {
+              transformOrigin = 'origin-bottom-right'
+              zIndex = 'z-1'
+            }
+          }
+        }
+
+        return (
+          <div
+            className={`absolute ${transformArgs} ${transformOrigin} ${zIndex} flex w-full h-full bg-contain bg-no-repeat bg-center drop-shadow-lg`}
+            style={{
+              backgroundImage: `url(${img})`,
+            }}
+          ></div>
+        )
+      })}
     </div>
   )
 }
