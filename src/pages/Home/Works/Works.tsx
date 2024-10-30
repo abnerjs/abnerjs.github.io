be3bc179c0a8931e6efddc19393c66701ee22f0f
abnerjs <abner.js05@gmail.com>
2024-10-30 17:59:38 -0300

    feat: turn jobs preview scalable

--- src/pages/Home/Works/Works.tsx
+++ src/pages/Home/Works/Works.tsx
@@ -19,6 +19,55 @@ import {
 } from 'src/assets/images/works'
 import ImageStack from 'src/components/ImageStack/ImageStack'
 
+interface IHoverPanel {
+  bgColor: string
+  images: any[]
+  type: 'desktop' | 'mobile' | 'both'
+  link: string
+  title: string
+  tool: string
+  dev: string
+}
+
+const jobs: IHoverPanel[] = [
+  {
+    bgColor: '#C3CCE1',
+    images: PortalRelatoriosAssets,
+    type: 'both',
+    link: 'https://github.com/abnerjs/PortalRelatorios',
+    title: 'Portal de Relatórios',
+    tool: 'React + .NET',
+    dev: 'Design & Desenvolvimento',
+  },
+  {
+    bgColor: '#D4B492',
+    images: SwingmdAssets,
+    type: 'desktop',
+    link: 'https://github.com/abnerjs/SwingMaterialDesign',
+    title: 'Material Design',
+    tool: 'Java Swing',
+    dev: 'Desenvolvimento de Componentes',
+  },
+  {
+    bgColor: '#D4CAC6',
+    images: PontoAssets,
+    type: 'both',
+    link: 'https://www.figma.com/design/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=1942-10750&t=5vr7MhWFxPzpHoHU-1',
+    title: 'Sistema de Ponto',
+    tool: 'Figma',
+    dev: 'Design & Interação',
+  },
+  {
+    bgColor: '#57A6A2',
+    images: IntegraAssets,
+    type: 'desktop',
+    link: 'https://www.behance.net/gallery/95516641/Integra-party-corona',
+    title: 'Identidade Visual',
+    tool: 'Photoshop',
+    dev: 'Design',
+  },
+]
+
 const HoverPanel = (props: any) => {
   return (
     <div
@@ -39,232 +88,72 @@ const HoverPanel = (props: any) => {
           transition: 'all ease 0.4s',
         }}
       >
-        <div
-          className='subPanel'
-          style={{
-            width: '100%',
-            height: '25%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#C3CCE1',
-          }}
-        >
-          <ImageStack
-            images={PortalRelatoriosAssets}
-            type='desktop'
-          />
-        </div>
-        <div
-          className='subPanel'
-          style={{
-            width: '100%',
-            height: '25%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#D4B492',
-          }}
-        >
-          <ImageStack
-            images={SwingmdAssets}
-            type='desktop'
-          />
-        </div>
-        <div
-          className='subPanel'
-          style={{
-            width: '100%',
-            height: '25%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#D4CAC6',
-          }}
-        >
-          <ImageStack
-            images={PontoAssets}
-            type='both'
-          />
-        </div>
-        <div
-          className='subPanel'
-          style={{
-            width: '100%',
-            height: '25%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#57A6A2',
-          }}
-        >
-          <ImageStack
-            images={IntegraAssets}
-            type='desktop'
-          />
-        </div>
+        {jobs.map((job) => (
+          <div
+            className='subPanel'
+            style={{
+              width: '100%',
+              height: '25%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: job.bgColor,
+            }}
+          >
+            <ImageStack
+              images={job.images}
+              type={job.type}
+            />
+          </div>
+        ))}
       </div>
     </div>
   )
 }
 
 const WorksContent = (props: any) => {
-  const elemPortalRelat = useRef<HTMLDivElement>(null)
-  const elemSwingMaterial = useRef<HTMLDivElement>(null)
-  const elemPonto = useRef<HTMLDivElement>(null)
-  const elemParty = useRef<HTMLDivElement>(null)
+  const elem = useRef<HTMLDivElement>(null)
+  const elem1 = useRef<HTMLDivElement>(null)
+  const elem2 = useRef<HTMLDivElement>(null)
+  const elem3 = useRef<HTMLDivElement>(null)
+
+  const elems = [elem, elem1, elem2, elem3]
+
   return (
     <>
-      <div
-        ref={elemPortalRelat}
-        className='content'
-        onMouseEnter={() => {
-          props.setHoverIndex(0)
-          props.setScale(1)
-        }}
-        onMouseLeave={() => props.setScale(0)}
-        onClick={() => {
-          window.open('https://github.com/abnerjs/PortalRelatorios')
-          window.open(
-            'https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1',
-            'blank'
-          )
-        }}
-        onMouseDown={(e) => {
-          if (e.button === 1) {
-            window.open('https://github.com/abnerjs/PortalRelatorios', 'blank')
-            window.open(
-              'https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1',
-              'blank'
-            )
-          }
-        }}
-      >
-        <MenuContext
-          targetId={elemPortalRelat.current!}
-          link={'https://github.com/abnerjs/PortalRelatorios'}
-          link2={
-            'https://www.figma.com/file/3AVOpouhDNUlUjsqSJcFdv/Portal-de-Relat%C3%B3rios?node-id=0%3A1&t=tLE4Z9AefrNkrbHo-1'
-          }
-        />
-        <div className='text'>Portal de Relatórios</div>
-        <div className='info'>
-          <div className='type'>React + .NET</div>
-          <div className='dev'>Design & Desenvolvimento</div>
-        </div>
-        <div className='container'></div>
-      </div>
-
-      <hr />
-
-      <div
-        ref={elemSwingMaterial}
-        className='content'
-        onMouseEnter={() => {
-          props.setHoverIndex(1)
-          props.setScale(1)
-        }}
-        onMouseLeave={() => props.setScale(0)}
-        onClick={() =>
-          window.open('https://github.com/abnerjs/SwingMaterialDesign')
-        }
-        onMouseDown={(e) => {
-          if (e.button === 1) {
-            window.open(
-              'https://github.com/abnerjs/SwingMaterialDesign',
-              'blank'
-            )
-          }
-        }}
-      >
-        <MenuContext
-          targetId={elemSwingMaterial.current!}
-          link={'https://github.com/abnerjs/SwingMaterialDesign'}
-        />
-        <div className='text'>Material Design</div>
-        <div className='info'>
-          <div className='type'>Java Swing</div>
-          <div className='dev'>Desenvolvimento de Componentes</div>
-        </div>
-      </div>
-
-      <hr />
-
-      <div
-        ref={elemPonto}
-        className='content mobile'
-        onMouseEnter={() => {
-          props.setHoverIndex(2)
-          props.setScale(1)
-        }}
-        onMouseLeave={() => props.setScale(0)}
-        onClick={() => {
-          window.open(
-            'https://www.figma.com/design/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=1942-10750&t=5vr7MhWFxPzpHoHU-1'
-          )
-        }}
-        onMouseDown={(e) => {
-          if (e.button === 1) {
-            window.open(
-              'https://www.figma.com/design/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=1942-10750&t=5vr7MhWFxPzpHoHU-1',
-              'blank'
-            )
-            window.open(
-              'https://www.figma.com/design/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=1942-10750&t=5vr7MhWFxPzpHoHU-1',
-              'blank'
-            )
-          }
-        }}
-      >
-        <MenuContext
-          targetId={elemPonto.current!}
-          link={
-            'https://www.figma.com/design/QkJJWaMoIeOkxN9UcDMWkc/Sistema-de-Ponto?node-id=1942-10750&t=5vr7MhWFxPzpHoHU-1'
-          }
-        />
-        <div className='text'>Sistema de Ponto</div>
-        <div className='info'>
-          <div className='type'>Figma</div>
-          <div className='dev'>Design & Interação</div>
-        </div>
-      </div>
-
-      <hr className='mobile' />
-
-      <div
-        ref={elemParty}
-        className='content mobile'
-        onMouseEnter={() => {
-          props.setHoverIndex(3)
-          props.setScale(1)
-        }}
-        onMouseLeave={() => props.setScale(0)}
-        onClick={() =>
-          window.open(
-            'https://www.behance.net/gallery/95516641/Integra-party-corona'
-          )
-        }
-        onMouseDown={(e) => {
-          if (e.button === 1) {
-            window.open(
-              'https://www.behance.net/gallery/95516641/Integra-party-corona',
-              'blank'
-            )
-          }
-        }}
-      >
-        <MenuContext
-          targetId={elemParty.current!}
-          link={'https://www.behance.net/gallery/95516641/Integra-party-corona'}
-        />
-        <div className='text'>Identidade Visual</div>
-        <div className='info'>
-          <div className='type'>Photoshop</div>
-          <div className='dev'>Design</div>
-        </div>
-      </div>
-
-      <hr className='mobile' />
+      {jobs.map((job, index) => (
+        <>
+          <div
+            ref={elems[index]}
+            className='content'
+            onMouseEnter={() => {
+              props.setHoverIndex(index)
+              props.setScale(1)
+            }}
+            onMouseLeave={() => props.setScale(0)}
+            onClick={() => {
+              window.open(job.link)
+            }}
+            onMouseDown={(e) => {
+              if (e.button === 1) {
+                window.open(job.link, 'blank')
+              }
+            }}
+          >
+            <MenuContext
+              targetId={elems[index].current!}
+              link={job.link}
+            />
+            <div className='text'>{job.title}</div>
+            <div className='info'>
+              <div className='type'>{job.tool}</div>
+              <div className='dev'>{job.dev}</div>
+            </div>
+            <div className='container'></div>
+          </div>
+          <hr className={index > 1 ? 'mobile' : ''} />
+        </>
+      ))}
     </>
   )
 }
